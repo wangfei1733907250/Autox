79fcb5d057948085069e9d05dc947b2ad300f0b6
aiselp <aiselpk@gmail.com>
2024-11-20 18:51:25 +0800

    定时任务页面更新

--- common/src/main/java/com/stardust/toast.kt
+++ common/src/main/java/com/stardust/toast.kt
@@ -1,30 +1,21 @@
 package com.stardust
 
 import android.content.Context
-import android.util.Log
 import android.widget.Toast
 import androidx.annotation.StringRes
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import java.lang.ref.WeakReference
-
-private var toast: WeakReference<Toast>? = null
 
 fun toast(context: Context, @StringRes resId: Int, isLongToast: Boolean = false) {
     toast(context, context.getString(resId), isLongToast)
 }
 
 fun toast(context: Context, text: String?, isLongToast: Boolean = false) {
-    Log.d("toast: ", "toast is null: ${toast?.get() == null}")
-    toast?.get()?.cancel()//
-    toast = WeakReference(
-        Toast.makeText(
-            context.applicationContext,
-            text,
-            if (isLongToast) Toast.LENGTH_LONG else Toast.LENGTH_SHORT
-        )
-    )
-    toast?.get()?.show()
+    Toast.makeText(
+        context.applicationContext,
+        text,
+        if (isLongToast) Toast.LENGTH_LONG else Toast.LENGTH_SHORT
+    ).show()
 }
 
 suspend fun String.toast(context: Context, isLongToast: Boolean = false) =
